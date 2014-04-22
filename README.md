1da28609d447c808e676e3ea486e49667820006a
flores8 <flores.lauralee@gmail.com>
2014-04-22 13:46:38 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+== Wiki
+
+Wiki that allows users to create public and private Markdown-based wikis.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wiki::Application.config.secret_key_base = '376c6866e6d5f6c1e14ac63f0977f1a4881b9bf2c23b12c40e7f83abe28b1a39b119b4a9f620c71714767e08cc2aa65d5235626c2651d11f53871c840df17989'
