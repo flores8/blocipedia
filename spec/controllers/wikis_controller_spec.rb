24fe5159f4f22c0f661699dcd35c41657c0d1d95
flores8 <flores.lauralee@gmail.com>
2014-04-23 12:09:14 -0700

    Created empty controller test with most important items to test

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -2,39 +2,62 @@ require 'spec_helper'
 
 describe WikisController do
 
-  describe "GET /wikis/:id" do
-    it "returns a 200" do
-      get :show
-      response.should be_success
+  # Index -> Show a list of wikis
+  describe "GET 'index' (/wikis)" do
+    it "shows a list of wikis" do
     end
-  end
 
-  describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
     end
   end
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+  # Show -> Shows the individual wiki
+  describe "GET 'show'(/wikis/:id)" do
+    it "shows the individual wiki" do
     end
   end
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+  # Create -> New wikis are created
+  describe "POST 'create'" do
+    context "with valid attributes" do 
+      it "creates a new wiki in the database" do
+      end
+      it "redirects to the @wiki" do
+      end
+    end
+
+    context "with invalid attributes" do 
+      it "does not save the wiki in the database" do
+      end
+      it "renders the :new template" do
+      end
     end
   end
 
-  describe "GET 'edit'" do
-    it "returns http success" do
-      get 'edit'
-      response.should be_success
+  # Update -> Can update wikis
+  describe "PATCH 'edit' or 'update'" do
+    context "with valid attributes" do 
+      it "updates the wiki in the database" do
+      end
+      it "redirects to the @wiki" do
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not update the wiki in the database" do
+      end
+      it "renders the :edit tempalate" do
+      end
     end
   end
 
+  # Destroy -> Can destroy wikis
+  describe "DELETE 'destroy'" do 
+    it "removes the wiki from the database" do
+    end
+    it "redirects to the wikis index page" do
+    end
+  end
 end
