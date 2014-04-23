8fa8b9915b85cea6056599b1fb1ffc3dede1eca3
flores8 <flores.lauralee@gmail.com>
2014-04-23 11:29:06 -0700

    added association between user and wikis

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  # Associations
+  has_many :wikis, dependent: :destroy
 end
