2fe997e2d0893f1aeb74f82e3dda318c350a5b6c
flores8 <flores.lauralee@gmail.com>
2014-04-25 10:55:44 -0700

    fixed production config for sending confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,5 @@ Blocipedia::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'blocipedias.herokuapp.com' }
 end
