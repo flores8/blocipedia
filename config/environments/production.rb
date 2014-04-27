43eefdc17e89c2c356c1d90d18910cccf0686825
flores8 <flores.lauralee@gmail.com>
2014-04-26 20:59:41 -0700

    Updated config file in production to try to fix heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,5 @@ Blocipedia::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  config.action_mailer.default_url_options = { host: 'blocipedias.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://blocipedias.herokuapp.com' }
 end
