fab81d9947eb95509ef5b6f084abc9ddea1f2373
flores8 <flores.lauralee@gmail.com>
2014-04-27 17:47:12 -0700

    Added markdown to post title and body

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,6 +3,8 @@ class Wiki < ActiveRecord::Base
   belongs_to :user
   has_many :pages, dependent: :destroy
 
+  default_scope { order('created_at DESC') }
+
   # Validations
   validates :name, presence: true
 end
