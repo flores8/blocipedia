7a2783f66629d06dd6a0ca116fede60e14567c5c
flores8 <flores.lauralee@gmail.com>
2014-05-07 10:39:01 -0700

    fixed error with viewing private wikis for those not logged in

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -4,11 +4,15 @@ class WikiPolicy < ApplicationPolicy
 
 	def show?
 		#binding.pry
-		collaborator = @user.collaborators.where(wiki_id: @record.id).first
-		owner = @user.collaborators.where(user_id: @user.id, wiki_id: @record.id).first
-		if collaborator.present?
-			collaborator.role = "owner" ? true : false
-			collaborator.role = "collaborator" ? true : false
+		if user.present?
+			collaborator = @user.collaborators.where(wiki_id: @record.id).first
+			owner = @user.collaborators.where(user_id: @user.id, wiki_id: @record.id).first
+			if collaborator.present?
+				collaborator.role = "owner" ? true : false
+				collaborator.role = "collaborator" ? true : false
+			else
+				false
+			end
 		else
 			false
 		end
